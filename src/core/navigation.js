9b596f9e66c3852fb7a52a69e494dd44e569872a
rinku-k <rinkuk.official@gmail.com>
2023-07-28 22:08:47 +0530

    theme remove

--- src/core/navigation.js
+++ src/core/navigation.js
@@ -4,9 +4,7 @@ import React from 'react';
 
 import { ROUTES } from '../constant';
 
-import { useColorScheme } from 'react-native';
 import { Adoption } from '../modules/adoption';
-import LightTheme, { DarkTheme } from '../modules/constants/themes';
 import { Home } from '../modules/home';
 import { Carousal } from '../modules/login/carousal';
 import { Profile } from '../modules/profile';
@@ -15,20 +13,9 @@ import { Splash } from '../modules/splash';
 const Stack = createNativeStackNavigator();
 
 export const Navigator = () => {
-  const scheme = useColorScheme();
-
   return (
-    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : LightTheme}>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {
-            // backgroundColor: 'rgba(255, 255, 255, 0.5)',
-          },
-          // headerTransparent: true,
-          headerTitleStyle: {
-            color: 'white',
-          },
-        }}>
+    <NavigationContainer>
+      <Stack.Navigator>
         <Stack.Screen
           name={ROUTES.Splash}
           component={Splash}
